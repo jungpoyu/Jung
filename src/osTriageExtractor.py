943ec7b0fbbfcd141758ddac5b6ff532dfe4e890
Owen Ou <oeg8168@gmail.com>
2019-11-25 22:19:15 +0800

    add osTriageExtractor.py (main program)

---
+++ src/osTriageExtractor.py
@@ -0,0 +1,12 @@
+from modules.report_finder import ReportFinder
+from modules.report_zipper import ReportZipper
+from modules.report_parser import ReportParser
+from modules.result_uploader import ResultUploader
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
