f9e7b683b86a80b0aa3c39b56f6f7ba460604966
Owen Ou <oeg8168@gmail.com>
2019-11-26 23:04:55 +0800

    add docstrings for make_zip() in ReportZipper

--- src/modules/report_zipper.py
+++ src/modules/report_zipper.py
@@ -6,4 +6,11 @@ class ReportZipper:
         pass
 
     def make_zip(self, folder_path, output_path):
+        """
+        Make zipped file from given folder path
+
+        Arguments:
+            folder_path {string} -- input folder path
+            output_path {string} -- output zipped file path (without .zip extension)
+        """
         shutil.make_archive(output_path, "zip", folder_path)
