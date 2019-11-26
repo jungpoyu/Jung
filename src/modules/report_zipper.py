ba0646e24ce202952ecccba7a9b3ae59a6d6e4af
Owen Ou <oeg8168@gmail.com>
2019-11-26 23:04:31 +0800

    implement make_zip() in ReportZipper

--- src/modules/report_zipper.py
+++ src/modules/report_zipper.py
@@ -1,3 +1,9 @@
+import shutil
+
+
 class ReportZipper:
     def __init__(self):
         pass
+
+    def make_zip(self, folder_path, output_path):
+        shutil.make_archive(output_path, "zip", folder_path)
