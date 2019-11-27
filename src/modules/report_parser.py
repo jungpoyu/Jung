27f4e7855604bccb5dd31d72d28b994eefb6ac27
Owen Ou <oeg8168@gmail.com>
2019-11-27 23:58:25 +0800

    use constructor of ReportParser to generate BeautifulSoup object for further use

--- src/modules/report_parser.py
+++ src/modules/report_parser.py
@@ -1,3 +1,14 @@
+from bs4 import BeautifulSoup
+
+CHUNK_SIZE = 4096
+
+
 class ReportParser:
-    def __init__(self):
-        pass
+    def __init__(self, folder_path):
+        content_path = f"{folder_path}/_meta/content.html"
+        with open(content_path, encoding="utf-8") as f:
+            data = ""
+            for chunk in iter(lambda: f.read(CHUNK_SIZE), f""):
+                data += chunk
+
+        self.soup = BeautifulSoup(data, "html.parser")
