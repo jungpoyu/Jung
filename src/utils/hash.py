dac380bb184cf04e039e334cf890541bdecaaebf
Owen Ou <oeg8168@gmail.com>
2019-12-04 21:46:01 +0800

    add docstrings for get_md5() and get_sha256() in hash utility

--- src/utils/hash.py
+++ src/utils/hash.py
@@ -4,6 +4,15 @@ CHUNK_SIZE = 4096
 
 
 def get_md5(file_path):
+    """
+    Hash a file using MD5 algorithm
+
+    Arguments:
+        file_path {string} -- input file path
+
+    Returns:
+        [string] -- file hash
+    """
     hasher = hashlib.md5()
 
     with open(file_path, "rb") as f:
@@ -14,6 +23,15 @@ def get_md5(file_path):
 
 
 def get_sha256(file_path):
+    """
+    Hash a file using SHA-256 algorithm
+
+    Arguments:
+        file_path {string} -- input file path
+
+    Returns:
+        [string] -- file hash
+    """
     hasher = hashlib.sha256()
 
     with open(file_path, "rb") as f:
