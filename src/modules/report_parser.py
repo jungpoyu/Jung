e5d6104262df66b2f03f204282caf6aa1160cfe4
Owen Ou <oeg8168@gmail.com>
2019-12-04 22:32:23 +0800

    implement parser functions for basic case informations (investigator, subject name, computer name, case number) in ReportParser

--- src/modules/report_parser.py
+++ src/modules/report_parser.py
@@ -12,3 +12,35 @@ class ReportParser:
                 data += chunk
 
         self.soup = BeautifulSoup(data, "html.parser")
+
+    def get_investigator(self):
+        return (
+            self.soup.find("td", text="Investigator:")
+            .find_next_sibling()
+            .find("div")
+            .text
+        )
+
+    def get_subject_name(self):
+        return (
+            self.soup.find("td", text="Subject name:")
+            .find_next_sibling()
+            .find("div")
+            .text
+        )
+
+    def get_computer_name(self):
+        return (
+            self.soup.find("td", text="Computer name:")
+            .find_next_sibling()
+            .find("div")
+            .text
+        )
+
+    def get_case_number(self):
+        return (
+            self.soup.find("td", text="Case number:")
+            .find_next_sibling()
+            .find("div")
+            .text
+        )
