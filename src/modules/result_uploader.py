d0d3e2eb441bca062749e66e5f1f9a44eca855d8
Owen Ou <oeg8168@gmail.com>
2020-02-17 19:29:07 +0800

    add dataset name, table name and table schema in ResultUploader

--- src/modules/result_uploader.py
+++ src/modules/result_uploader.py
@@ -1,3 +1,21 @@
 class ResultUploader:
+    _dataset = "Jung"
+    _table = "Evidence_V1"
+    _schema = [
+        {"name": "CaseID", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "EvidenceType", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "EvidenceID", "type": "INTEGER", "mode": "REQUIRED"},
+        {"name": "SeizureLocation", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "Perpetrator", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "EvidenceDescription", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "EvidenceHash", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "Quantity", "type": "INTEGER", "mode": "REQUIRED"},
+        {"name": "Investigator", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "SearchTimestamp", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "TimeZone", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "ZipHash", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "IsTransferred", "type": "BOOLEAN", "mode": "REQUIRED"},
+    ]
+
     def __init__(self):
         pass
